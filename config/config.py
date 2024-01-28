9e1f1058b54353f003711f23c0f59edf2a2a57de
MartinWie <donotsuspend@googlegroups.com>
2024-01-28 16:34:20 +0100

    Added Slack bot token loading and validation to the configuration.

--- config/config.py
+++ config/config.py
@@ -12,3 +12,14 @@ if OPENAI_API_KEY is None:
     raise Exception("OPENAI_API_KEY is not set in the environment variables.")
 else:
     log_to_aws(LogLevel.INFO, "OPENAI_API_KEY loaded from environment variables.")
+
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+
+if SLACK_BOT_TOKEN is None:
+    log_to_aws(
+        LogLevel.ERROR,
+        "SLACK_BOT_TOKEN is not set in the environment variables. It needs to be set for this lambda function.",
+    )
+    raise Exception("SLACK_BOT_TOKEN is not set in the environment variables.")
+else:
+    log_to_aws(LogLevel.INFO, "SLACK_BOT_TOKEN loaded from environment variables.")
