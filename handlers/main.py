dc3e92449cc42c6a811dc3892a1fcbfca4479671
MartinWie <donotsuspend@googlegroups.com>
2024-01-29 23:00:44 +0100

    Refactored URL verification to parse event body from JSON and improved logging.

--- handlers/main.py
+++ handlers/main.py
@@ -1,3 +1,5 @@
+import json
+
 from util.logger import log_to_aws, LogLevel
 
 
@@ -5,18 +7,23 @@ def lambda_handler(event, context):
     log_to_aws(LogLevel.INFO, "Lambda function invoked!")
     log_to_aws(LogLevel.INFO, f"Received event: {event}")
 
-    if 'type' in event and event['type'] == 'url_verification':
-        log_to_aws(LogLevel.INFO, "URL verification event received.")
-        if 'challenge' in event:
-            challenge = event['challenge']
-            log_to_aws(LogLevel.INFO, f"Responding with challenge: {challenge}")
-            return {
-                "statusCode": 200,
-                "headers": {
-                    "Content-Type": "text/plain"
-                },
-                "body": challenge
-            }
+    # Check if 'body' exists in the event
+    if 'body' in event:
+        # Parse the event body from JSON
+        body = json.loads(event['body'])
+
+        if 'type' in body and body['type'] == 'url_verification':
+            log_to_aws(LogLevel.INFO, "URL verification event received.")
+            if 'challenge' in body:
+                challenge = body['challenge']
+                log_to_aws(LogLevel.INFO, f"Responding with challenge: {challenge}")
+                return {
+                    "statusCode": 200,
+                    "headers": {
+                        "Content-Type": "text/plain"
+                    },
+                    "body": challenge
+                }
 
     return {
         "statusCode": 200,
