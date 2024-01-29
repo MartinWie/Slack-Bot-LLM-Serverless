af5c11591552b5837bad6e82a8389641b07cb7d1
MartinWie <donotsuspend@googlegroups.com>
2024-01-29 23:29:27 +0100

    Enhanced Slack bot response functionality and updated dependencies.

--- handlers/main.py
+++ handlers/main.py
@@ -1,6 +1,29 @@
 import json
+import os
+
+from six.moves import urllib
 
 from util.logger import log_to_aws, LogLevel
+
+
+# Function to send a text response to Slack
+def send_text_response(event, response_text):
+    SLACK_URL = "https://slack.com/api/chat.postMessage"
+    channel_id = event["event"]["channel"]
+    user = event["event"]["user"]
+    bot_token = os.environ.get("SLACK_BOT_TOKEN")
+    data = urllib.parse.urlencode({
+        "token": bot_token,
+        "channel": channel_id,
+        "text": response_text,
+        "user": user,
+        "link_names": True
+    })
+    data = data.encode("ascii")
+    request = urllib.request.Request(SLACK_URL, data=data, method="POST")
+    request.add_header("Content-Type", "application/x-www-form-urlencoded")
+    res = urllib.request.urlopen(request).read()
+    log_to_aws(LogLevel.INFO, f'Response from Slack: {res}')
 
 
 def lambda_handler(event, context):
@@ -25,7 +48,17 @@ def lambda_handler(event, context):
                     "body": challenge
                 }
 
+        elif 'type' in body and body['type'] == 'event_callback':
+            if 'event' in body and 'type' in body['event'] and body['event']['type'] == 'message':
+                message_text = body['event'].get('text', '')
+                if '<@U06GBCG8E9F>' in message_text or 'Leela' in message_text:
+                    send_text_response(body, "ok")
+                    return {
+                        "statusCode": 200,
+                        "body": "Response sent"
+                    }
+
     return {
         "statusCode": 200,
-        "body": "ok"
+        "body": "Event type not supported"
     }
