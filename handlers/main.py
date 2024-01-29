96fe20be6c17d27b53f24201a9fb03e6ebe54269
MartinWie <donotsuspend@googlegroups.com>
2024-01-29 22:12:17 +0100

    Refactored lambda_handler to support Slack URL verification events and fixed typo in log message.

--- handlers/main.py
+++ handlers/main.py
@@ -2,7 +2,23 @@ from util.logger import log_to_aws, LogLevel
 
 
 def lambda_handler(event, context):
-    log_to_aws(LogLevel.INFO, f"Lambda function invoced!")
+    log_to_aws(LogLevel.INFO, "Lambda function invoked!")
 
-    log_to_aws(LogLevel.INFO, f"Lambda function finished!")
-    return {"response": "ok"}
+    if 'type' in event and event['type'] == 'url_verification':
+        log_to_aws(LogLevel.INFO, "URL verification event received.")
+        if 'challenge' in event:
+            challenge = event['challenge']
+            log_to_aws(LogLevel.INFO, f"Responding with challenge: {challenge}")
+            return {
+                "statusCode": 200,
+                "headers": {
+                    "Content-Type": "text/plain"
+                },
+                "body": challenge
+            }
+        else:
+            log_to_aws(LogLevel.ERROR, "No challenge in event.")
+            return {
+                "statusCode": 400,
+                "body": "Bad Request"
+            }
